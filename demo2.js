13390bda0295190d5cfe3e7e5b100f088db12ca0
smokeybear87 <alex@de-facto.com>
2012-12-20 16:22:18 +0000

    adds persistent filesystem support

--- demo2.js
+++ demo2.js
@@ -8,7 +8,7 @@
 
 	function createTempFile(callback) {
 		var tmpFilename = "tmp.dat";
-		requestFileSystem(TEMPORARY, 4 * 1024 * 1024 * 1024, function(filesystem) {
+		requestFileSystem(LocalFileSystem.PERSISTENT, 4 * 1024 * 1024 * 1024, function(filesystem) {
 			function create() {
 				filesystem.root.getFile(tmpFilename, {
 					create : true
