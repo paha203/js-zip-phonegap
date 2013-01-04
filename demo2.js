3791c3ef468c5cecd098cff34afa41fc986534a5
Nathan Edwards <nedwards@de-facto.com>
2013-01-04 15:19:49 +0000

    foo

--- demo2.js
+++ demo2.js
@@ -47,7 +47,7 @@ var fetchData = function() {
 								var fileReader = new FileReader;
 								fileReader.onload = function(){
 									log('onload called, starting to write the result');
-									log(fileReader.result);
+									//log(fileReader.result);
 									//fileWriter.write(fileReader.result);
 								}
 								log('starting to read as text');
