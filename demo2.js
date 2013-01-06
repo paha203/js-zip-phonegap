d621a6e97a97e71b7324b333187d1465d5b7c774
Nathan Edwards <nedwards@de-facto.com>
2013-01-06 16:01:48 +0000

    woo timeouts lolzelz

--- demo2.js
+++ demo2.js
@@ -4,12 +4,13 @@ log = function(str){
 
 log('waiting for device ready');
 
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('deviceready', function(){
     view = new Status('status');
 
     log('device ready fired');
 
-    window.webkitRequestFileSystem(TEMPORARY, 1024 * 1024 * 1024 * 5, function(filesystem){
+//    window.webkitRequestFileSystem(LocalFileSystem.PERSISTENT, 1024 * 1024 * 1024 * 5, function(filesystem){
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(filesystem){
         fs = filesystem.root;
         log('filesystem name is: '+filesystem.name);
         controller = new Controller(view);
